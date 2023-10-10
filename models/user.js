566d0312916a74b18f26b227cb4f30585716fa70
Egor Sinko <cowboy.killerrr@gmail.com>
2023-10-10 19:04:01 +1000

    feat: созданы схемы для моделей user и card

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
