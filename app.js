f5e1e91a36035f38e56f1d08d8f2bd62c02a3814
Egor Sinko <cowboy.killerrr@gmail.com>
2023-10-11 20:00:22 +1000

    feat: добавлена функция создания пользователя

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb', {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((error) => console.log(`Произошла ошибка: ${error}`));
+
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '65266fc4d989a5ce24cde5c6',
+  };
+
+  next();
 });
 
+app.use('/users', require('./routes/users'));
+
 app.listen(PORT, () => {
-  console.log(`App listening on http://localhost:${PORT}/`);
+  console.log(`Server is running on: http://localhost:${PORT}/`);
 });
---
+++ controllers/users.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Server Error' }));
+};
+
+const getUser = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send({ user }))
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        return res.status(400).send({ message: error.message });
+      }
+
+      return res.status(500).send({ message: 'Server Error' });
+    });
+};
+
+module.exports = { getUsers, getUser, createUser };
