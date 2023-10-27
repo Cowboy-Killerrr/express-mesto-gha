f7759a7ecdac59c1a3e5ac30b479a6e86f020cef
Egor Sinko <cowboy.killerrr@gmail.com>
2023-10-27 21:32:56 +1000

    fix: исправлен баг
    Некорректный email отлавливается при помощи Joi

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
     avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
   }),
 }), createUser);
